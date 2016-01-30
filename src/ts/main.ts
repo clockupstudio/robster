d397a43b91ffc8b7c12b71dff4bc35848ffd015f
Kritsadanshon Sadeewong <kritsadanshon@gmail.com>
2016-01-30 13:12:30 +0700

    player can move left and right

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -4,11 +4,15 @@ document.addEventListener('DOMContentLoaded', () => display());
 
 var game;
 var player;
+var leftKey;
+var rightKey;
+var stepMove = 2.5;
 
 function display() {
     game = new Phaser.Game(1280, 720, Phaser.AUTO, 'robster', {
     preload: preload,
     create: create,
+    update: update,
     });
 }
 
@@ -18,10 +22,29 @@ function preload() {
 
 function create() {
     initPlayer();
+  
+    leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+    rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
 }
 
 function initPlayer() {
     player = game.add.sprite(160, 640, 'player');
     player.anchor.x = 0;
     player.anchor.y = 1;
+}
+
+function update() {
+    if (leftKey.isDown)
+    {
+        player.anchor.x = 1;
+        player.scale.x = -1;
+        player.x = player.x - stepMove ;
+        
+    }
+    else if (rightKey.isDown)
+    {
+        player.anchor.x = 0;
+        player.scale.x = 1;
+        player.x = player.x + stepMove;
+    }
 }
